a886c47d7ccd89ee529a1ea5a66bf114351e65d4
lengon <lengon@DESKTOP-P6TQR4I>
2019-03-13 16:57:49 +0800

    链表相加的算法

---
+++ doc/002/readme.md
@@ -0,0 +1,7 @@
+原题
+　　You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list. 
+　　Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) 
+　　Output: 7 -> 0 -> 8 
+
+题目大意
+　　有两个单链表，代表两个非负数，每一个节点代表一个数位，数字是反向存储的，即第一个结点表示最低位，最后一个结点表示最高位。求两个数的相加和，并且以链表形式返回。 
---
+++ src/main/java/article/A002Node.java
@@ -0,0 +1,27 @@
+package article;
+
+public class A002Node {
+	public int value;
+	public A002Node next;
+	
+	public A002Node(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public A002Node getNext() {
+		return next;
+	}
+
+	public void setNext(A002Node next) {
+		this.next = next;
+	}
+
+}
