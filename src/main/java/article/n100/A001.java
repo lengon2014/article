114c6ec177de0cc0a1f1ee27ef404d22e0171146
lengon <lengon@DESKTOP-P6TQR4I>
2019-03-28 17:34:11 +0800

    28

--- src/main/java/article/n100/A001.java
+++ src/main/java/article/n100/A001.java
@@ -6,7 +6,7 @@ public class A001 {
 		int[] input = new int[] { 2, 11, 7, 15 };
 		int target = 9;
 		int[] result = buildIndex(input, target);
-		System.out.println(buildInfo(result));
+		System.out.println(ArrayUtils.buildInfo(result));
 	}
 
 	public static int[] buildIndex(int[] input, int target) {
@@ -19,13 +19,5 @@ public class A001 {
 		}
 		
 		return null;
-	}
-	
-	public static String buildInfo(int[] input) {
-		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<input.length;i++) {
-			sb.append("index: ").append(i).append(" value: ").append(input[i]).append("\n");
-		}
-		return sb.toString();
 	}
 }
